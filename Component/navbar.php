86f60036398263e00cd4e77d8a52d7cd93a9a65e
Krisna Wahyu Setyawan <exclusive.dec4@gmail.com>
2023-11-09 14:25:33 +0700

    init commit

---
+++ Component/navbar.php
@@ -0,0 +1,44 @@
+<nav class="border-emerald-200 bg-emerald-100 dark:bg-emerald-600 dark:border-emerald-700">
+    <div class="container mx-auto">
+        <div class="max-w-screen-3xl flex flex-wrap items-center justify-between mx-auto p-5">
+            <div class="flex items-center">
+                <a href="https://www.pens.ac.id" target="_blank">
+                    <img src="/Property/pens_icon.png" class="h-12 mr-3" alt="Logo PENS" />
+                </a>
+                <a href="https://sainsdata.pens.ac.id" target="_blank">
+                    <img src="/Property/sdt_icon.png" class="h-[45px] ml-2 " alt="Logo SDT" />
+                </a>
+            </div>
+            <button data-collapse-toggle="navbar-hamburger" type="button" class="inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-cyan-500 rounded-lg hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-200 dark:text-gray-400 dark:hover:bg-cyan-700 dark:focus:ring-cyan-600" aria-controls="navbar-hamburger" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+            <div class="hidden w-full" id="navbar-hamburger">
+                <ul class="flex flex-col font-medium mt-4 rounded-lg bg-emerald-50 dark:bg-emerald-600 dark:border-emerald-700">
+                    <li>
+                        <a href="/View/" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-green-100 dark:text-gray-400 dark:hover:bg-green-700 dark:hover:text-white">
+                            Home Page
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/View/Penjualan/" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-green-100 dark:text-gray-400 dark:hover:bg-green-700 dark:hover:text-white">
+                            Penjualan
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/View/Pembelian" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-green-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-green-700 dark:hover:text-white">
+                            Pembelian
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/View/Contact/" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-green-100 dark:text-gray-400 dark:hover:bg-green-700 dark:hover:text-white">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
